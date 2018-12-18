77641045e360fddea41f2c679912d5a15ff0ddf4
O'Kane <sokane@ic.ac.uk>
2018-12-18 18:40:56 +0000

    Updated Ds information using GITT data as opposed to EIS

--- Ds_of_cs_and_T_anode.m
+++ Ds_of_cs_and_T_anode.m
@@ -5,11 +5,11 @@ R = 8.3144; % universal gas constant, J mol^-1 K^-1
 
 x = cs/cmax;
 
-% Graphite anode from Ecker, Kabitz, Laresgoiti et al. (EIS data, not GITT)
+% Graphite anode from Ecker, Kabitz, Laresgoiti et al. (GITT data, not EIS)
 % Analytical fit (WebPlotDigitizer + MATLAB curve fiting tool)
 
 % First, calculate kappa(cs ,cmax, 296) using exponential fit to EIS data
-Ds_cs_296 = 9.728e-14*exp(-6.226*x);
+Ds_cs_296 = 2.275e-13*exp(-8.588*x);
 % Now add Arrhenius temperature dependence
 Ds = Ds_cs_296*exp(-30300/(R*T))*exp(30300/(R*296));
 
--- Ds_of_cs_and_T_cathode.m
+++ Ds_of_cs_and_T_cathode.m
@@ -9,6 +9,6 @@ x = cs/cmax;
 % Assume no cs dependence in the first instance
 
 % First, calculate kappa(cs ,cmax, 296) using exponential fit to EIS data
-Ds_cs_296 = 6.3e-13;
+Ds_cs_296 = 3.2e-13;
 % Now add Arrhenius temperature dependence
 Ds = Ds_cs_296*exp(-80600/(R*T))*exp(80600/(R*296));
